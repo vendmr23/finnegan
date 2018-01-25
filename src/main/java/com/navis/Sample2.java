8bd799f11890b01c4e73ae027a4c6287619fe942
vendmr <michael.robinson@navis.com>
2018-01-25 09:31:24 -0800

    Second comit

--- src/main/java/com/navis/Sample2.java
+++ src/main/java/com/navis/Sample2.java
@@ -12,6 +12,6 @@ package com.navis;
 public class Sample2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
