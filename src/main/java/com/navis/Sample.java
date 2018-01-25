033d8d583965492540a7ccafa0680cee9f1cfa02
vendmr <michael.robinson@navis.com>
2018-01-25 09:22:26 -0800

    first commit

---
+++ src/main/java/com/navis/Sample.java
@@ -0,0 +1,17 @@
+/*
+* Copyright (c) 2012 Navis LLC. All Rights Reserved.
+* $Id: $
+*/
+package com.navis;
+
+/**
+ * This class... todo javadoc
+ *
+ * @author <a href="mailto:xxx@navis.com">robinmi</a>, 1/25/2018
+ */
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
